523cdf1a945c1cc9d290bbcab639ce68aabeb3fe
Abdelrahman Gamal <33043163+AbdelrahmanGamal-jimy2@users.noreply.github.com>
2021-10-09 18:43:09 +0200

    added the Shared action creators

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,2 +1,25 @@
 import { _getUsers } from "../utils/_DATA";
 import { _getQuestions } from "../utils/_DATA";
+
+import { getUsers } from "./users";
+import { getQuestions } from "./questions";
+
+export function handleIntialUsers()
+{
+    return(dispatch)=>
+    {
+        return _getUsers().then(({users})=>{
+            dispatch(getUsers(users))
+        })
+    }
+}
+
+export function handleIntialQuestions()
+{
+    return(dispatch)=>
+    {
+        return _getQuestions().then(({questions})=>{
+            dispatch(getQuestions(questions))
+        })
+    }
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,18 +4,22 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 class Login extends Component
 {
+    handleLogin = (e)=>
+    {
+
+    }
     render()
     {
         return (
             <div>
                 <h1>Please select a user to login with.....</h1>
-                <Form.Select aria-label="Default select example">
+                <Form.Select onSubmit={this.handleLogin} aria-label="Default select example">
                     <option>Select a user</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
                 </Form.Select>
-                <Button variant="primary" type="submit" onClick = {this.handleLogin}>
+                <Button variant="primary" type="submit">
                     Login
                 </Button>
             </div>
